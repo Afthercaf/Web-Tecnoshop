d3026bcb26daa0f5465f727037d935020a4899cc
Afthercaf <145415218+Afthercaf@users.noreply.github.com>
2024-12-01 13:27:58 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,8 +12,9 @@ const app = express();
 
 app.use(
   cors({
+    origin: process.env.FRONTEND_URL, // Cambia esto a la URL de tu frontend en Vercel
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-    origin: '*',
   })
 );
 
